9feb8bc619af63f3cdb53e51d74fd75b0412a006
Matthew Horridge <matthew.horridge@stanford.edu>
2021-11-11 14:20:35 -0800

    Initial commit

---
+++ src/main/java/edu/stanford/protege/webprotege/filesub/WebProtegeFileSubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.stanford.protege.webprotege.filesub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProtegeFileSubmissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProtegeFileSubmissionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/stanford/protege/webprotege/filesub/WebProtegeFileSubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.stanford.protege.webprotege.filesub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProtegeFileSubmissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
